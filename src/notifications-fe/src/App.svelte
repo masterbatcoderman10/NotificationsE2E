84ab8544d1b29fefa7810acb758ddeb08912346b
masterbatcoderman10 <suhaib.athar@gmail.com>
2024-11-02 18:32:10 +0400

    added setup backend files

--- src/notifications-fe/src/App.svelte
+++ src/notifications-fe/src/App.svelte
@@ -1,5 +1,17 @@
 <script>
+  import { onMount } from "svelte";
   import "./app.css";
+
+  onMount(async () => {
+    const response = await fetch("http://localhost:3000/");
+    
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      console.error("Failed to fetch notifications");
+    }
+  });
 </script>
 
 <main class="">
